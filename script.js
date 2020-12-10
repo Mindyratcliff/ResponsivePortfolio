365e5f60b13139fcf2271a014546c8de8a7e992f
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2020-12-10 12:49:06 -0500

    Formatted html, css, and script

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
- $(document).ready(function(){
-        $('.toast').toast('show');
-      
-        $("button").click(function(){
-            $(".toast d-flex align-items-center").hide();
-            $(".LI-profile-badge").hide();
-        });
-    
+$(document).ready(function () {
+  $(".toast").toast("show");
 
-    });
-
+  $("button").click(function () {
+    $(".toast d-flex align-items-center").hide();
+    $(".LI-profile-badge").hide();
+  });
+});
